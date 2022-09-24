ca92ec4cf768739e4801e46d9f1da6317226d05e
Kenneth Munk <da.munkk@gmail.com>
2022-09-23 22:07:11 -0700

    phase 2 was completed... then broke

--- fancifymytext.js
+++ fancifymytext.js
@@ -1,7 +1,11 @@
-const textarea = {
-
+const textmodder = {
+	
 	bigger(){
 		alert("Hello, world!");
+		//document.getElementById("fancytext").style.fontSize = "24pt";
 	}
 	
+	fancySchmancy(){
+		alert("Hello, world!");
+	}
 }
