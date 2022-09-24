2bb5e1d885059bc3f914b3c292d3f3745f3cb6e5
Kenneth Munk <da.munkk@gmail.com>
2022-09-23 22:54:03 -0700

    Phase 3 complete

--- fancifymytext.js
+++ fancifymytext.js
@@ -1,11 +1,22 @@
-const textmodder = {
+
 	
-	bigger(){
-		alert("Hello, world!");
-		//document.getElementById("fancytext").style.fontSize = "24pt";
-	}
+function bigger(){
+	alert("Hello, world!");
 	
-	fancySchmancy(){
-		alert("Hello, world!");
-	}
-}
+	document.getElementById("fancytext").style.fontSize = "24px";
+	document.getElementById("boringRadial").checked = false;
+};
+
+function fancySchmancy(){
+	alert("Hello, world!");
+	document.getElementById("fancytext").style.fontWeight = "bold";
+	document.getElementById("boringRadial").checked = false;
+};
+
+function boringBetty(){
+	alert("Hello, world!");
+	document.getElementById("fancytext").style.fontWeight = "normal";
+	document.getElementById("fancytext").style.fontSize = "12px";
+	document.getElementById("fancyRadial").checked = false;
+};
+
